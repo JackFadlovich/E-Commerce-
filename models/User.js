518c9b3e20b742773d03b2ab90fc5bc71a486af6
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-07-22 19:41:10 -0500

    added bcrypt and hook

--- models/User.js
+++ models/User.js
@@ -1,30 +1,61 @@
 const { Model, DataTypes } = require('sequelize');
-
+const bcrypt = require('bcrypt');
 const sequelize = require('./config/connection');
 
+class User extends Model 
+{
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-user.init(
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+   
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+   
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+   
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+    },
+  {
+    hooks: 
     {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      beforeCreate: async (newUserData) => 
+        {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
       },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      
+      beforeUpdate: async (updatedUserData) => 
+        {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
-        },
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false }
-      }
-)
+    },
+    
+    sequelize,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
