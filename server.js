02e27bc93a5b185dab0526ad4560005fa055dd2f
GooGaru <xwilliam.morrisonx@gmail.com>
2024-07-23 19:59:50 -0400

    calling in handlebars

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const sequelize = require("./config/connections");
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 // Inform Express.js on which template engine to use
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
