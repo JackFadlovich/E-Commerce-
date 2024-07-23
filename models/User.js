9ee06950851046a892c651ad605db1ca420fb970
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-07-22 19:31:32 -0500

    started on user.js

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('./config/connection');
+
+
+user.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false }
+      }
+)
