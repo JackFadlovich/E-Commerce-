47f7ae5eb0419caaf6b368e6e954c4415db8e49a
Chris <ccolon29@mail.valenciacollege.edu>
2024-07-27 17:22:02 -0400

    fixed user post route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,18 +1,19 @@
 const router = require("express").Router();
-// const User = require('../../models/User');
+const User = require('../../models/User');
 const Product = require("../../models/Product");
-// router.post('/', async (req, res) => {
-//     const newUser = User.create({
-//         first_name: 'John',
-//         last_name: 'Doe',
-//         phone_number: '1234567890',
-//         user_name: 'JohnDoe',
-//         email: 'johndoe@gmail.com',
-//         password: 'john'
-//     });
 
-//     res.json(newUser);
-// });
+router.post('/', async (req, res) => {
+    const newUser = User.create({
+        first_name: 'John',
+        last_name: 'Doe',
+        phone_number: '1234567890',
+        user_name: 'JohnDoe',
+        email: 'johndoe@gmail.com',
+        password: 'john'
+    });
+
+    res.json(newUser);
+});
 
 router.post("/", async (req, res) => {
   const newProduct = Product.create({
