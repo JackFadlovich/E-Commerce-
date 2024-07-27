bc050c4aac9850b6a058acc678c2a8c04260a7b7
GooGaru <xwilliam.morrisonx@gmail.com>
2024-07-27 17:06:55 -0400

    Created product post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,17 +1,30 @@
 const router = require("express").Router();
-const User = require('../../models/User');
+// const User = require('../../models/User');
+const Product = require("../../models/Product");
+// router.post('/', async (req, res) => {
+//     const newUser = User.create({
+//         first_name: 'John',
+//         last_name: 'Doe',
+//         phone_number: '1234567890',
+//         user_name: 'JohnDoe',
+//         email: 'johndoe@gmail.com',
+//         password: 'john'
+//     });
 
-router.post('/', async (req, res) => {
-    const newUser = User.create({
-        first_name: 'John',
-        last_name: 'Doe',
-        phone_number: '1234567890',
-        user_name: 'JohnDoe',
-        email: 'johndoe@gmail.com',
-        password: 'john'
-    });
+//     res.json(newUser);
+// });
 
-    res.json(newUser);
+router.post("/", async (req, res) => {
+  const newProduct = Product.create({
+    product_id: "001",
+    product_name: "PlaceHolder",
+    category: "Tech",
+    product_description: "Technology",
+    price: "200",
+    manufacturer: "Logitech",
+    product_rating: "2",
+  });
+  res.json(newProduct);
 });
 
 module.exports = router;
