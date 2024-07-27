a5faf3dfe620ae66bb2e0dc6f09210b5abb6feb5
Chris <ccolon29@mail.valenciacollege.edu>
2024-07-27 15:43:21 -0400

    testing changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ router.post('/', async (req, res) => {
     const newUser = User.create({
         first_name: 'John',
         last_name: 'Doe',
-        phone_number: '123-456-7890',
+        phone_number: '1234567890',
         user_name: 'JohnDoe',
         email: 'johndoe@gmail.com',
         password: 'john'
