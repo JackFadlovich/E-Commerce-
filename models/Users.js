3832b3efd5479e604cfd567d75710aeb256255ab
Chris <ccolon29@mail.valenciacollege.edu>
2024-07-28 19:03:32 -0400

    made some small fixes. everything works up to GET requests.

--- models/Users.js
+++ models/Users.js
@@ -1,11 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class Users extends Model {
-  // checkPassword(loginPw) {
-  //   return bcrypt.compareSync(loginPw, this.password);
-  // }
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 Users.init(
@@ -40,10 +40,10 @@ Users.init(
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      // unique: false,
-      // validate: {
-      //   isEmail: true,
-      // },
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
 
     password: {
@@ -52,20 +52,20 @@ Users.init(
     },
   },
   {
-    // hooks: {
-    //   beforeCreate: async (newUserData) => {
-    //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
-    //     return newUserData;
-    //   },
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
 
-    //   beforeUpdate: async (updatedUserData) => {
-    //     updatedUserData.password = await bcrypt.hash(
-    //       updatedUserData.password,
-    //       10
-    //     );
-    //     return updatedUserData;
-    //   },
-    // },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
