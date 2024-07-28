ad252e785a56cc48f23b57140aff86277713fe0e
Chey <Cheyenne@Cheyennes-MacBook-Pro.local>
2024-07-28 16:41:18 -0400

    hi

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,23 @@
-const sequelize = require('../config/connections');
+const sequelize = require('../config/connection');
+const Product = require('../models/Product');
+const User = require('../models/User')
+
+const userData = require("./userData.json");
+const productData = require("./productData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Product.bulkCreate(productData);
+
+//    await User.build(userData, {
+//     individualHooks: true,
+//     returning: true,
+//    });
+
+
+   process.exit(0);
+
+}
+
+seedDatabase();
