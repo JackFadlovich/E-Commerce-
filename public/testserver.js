2689b4e7e0be38e190e1e3b07fe3fd8ababd5c6c
Mayena <mayenawardak03@icloud.com>
2024-07-29 17:28:18 -0400

    Finalize server sync and start in testServer.js

--- public/testserver.js
+++ public/testserver.js
@@ -23,3 +23,10 @@ app.use(express.urlencoded({ extended: true }));
 const authRoutes = require("../controllers/authRoutes"); 
 app.use("/auth", authRoutes);
 
+// Finalize server sync and start
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log("Test server started");
+    });
+});
+
