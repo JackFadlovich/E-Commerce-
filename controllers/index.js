e62a631243cacec21cdad516f123b98e5693fe3c
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-07-29 12:38:59 -0500

    boxes show up. still need to have them display data

--- controllers/index.js
+++ controllers/index.js
@@ -5,25 +5,31 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 
-const {Product} = require('../models'); // Ensure this matches the export in models/index.js
+const {Product, Buyer, Order, Seller, SellerProduct, Users} = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const products = await Product.findAll();
-    //const users = await User();
-    
-    // Render the data in the view
-    res.render('index', { products, //users 
+    const productData = await Product.findAll();
+    const userData = await Users.findAll();
+    const buyerData = await Buyer.findAll();
+    const orderData = await Order.findAll();
+    const sellerData = await Seller.findAll();
+    const sellerProductData = await SellerProduct.findAll();
 
-    });
+    // Serialize data so the template can read it
+    const products = productData.map(product => product.get({ plain: true }));
+    const users = userData.map(user => user.get({ plain: true }));
+    const buyers = buyerData.map(buyer => buyer.get({ plain: true }));
+    const orders = orderData.map(order => order.get({ plain: true }));
+    const sellers = sellerData.map(seller => seller.get({ plain: true }));
+    const sellerProducts = sellerProductData.map(sellerProduct => sellerProduct.get({ plain: true }));
+    // Render the data in the view
+    res.render('index.handlebars', { products, users, buyers, orders, sellers, sellerProducts });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
   }
 });
-
-
-
 
 
 
