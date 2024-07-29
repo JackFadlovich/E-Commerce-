f53ec72a8094d67294666abbbb4fce3938e70b30
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-07-29 10:22:31 -0500

    shows list of stuff

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,29 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+
+const {Product} = require('../models'); // Ensure this matches the export in models/index.js
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    //const users = await User();
+    
+    // Render the data in the view
+    res.render('index', { products, //users 
+
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
 module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 // Inform Express.js on which template engine to use
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //Calling in the models to create the databases
 const model = require('./models');
