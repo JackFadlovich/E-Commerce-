68bac47eb17471e721ed6081311934c1298fdf1f
Chris <ccolon29@mail.valenciacollege.edu>
2024-07-30 15:47:15 -0400

    fixed the /product page and now displays properly.

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,40 +1,6 @@
 const router = require("express").Router();
 const { Product } = require("../../models");
 
-//Get all products
-router.get("/", async (req, res) => {
-  try {
-    const allProducts = await Product.findAll({});
-    console.log("Im working");
-
-    const products = allProducts.map((product) => product.get({ plain: true})); 
-
-    res.render('product', {
-      products,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Get one product by name
-router.get("/:category", async (req, res) => {
-  Product.findByPk(req.params.category).then((productData) => {
-    res.json(productData);
-  });
-});
-//   try {
-//     const oneProductData = await Product.findByPk(req.params.category);
-//   //   if (!oneProductData) {
-//   //     res.status(404).json({ message: "No Product found!" });
-//   //     return;
-//   //   }
-//   //   res.status(200).json(oneProductData);
-//   // } catch (err) {
-//   //   res.status(500).json(err);
-//   }
-// });
-
 router.post("/", async (req, res) => {
   try {
     const newProduct = await Product.create({
