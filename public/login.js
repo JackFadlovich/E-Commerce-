565d73c37016c5ae48f93a25b753b40bfb0fddcb
GooGaru <xwilliam.morrisonx@gmail.com>
2024-07-29 22:45:39 -0400

    updated the login.js to match the login.handlebars naming conventions

--- public/login.js
+++ public/login.js
@@ -2,53 +2,56 @@ const loginFormHandler = async (event) => {
 event.preventDefault();
 
 //Collect values from the login form
-const email = document.querySelector('#email-login').value.trim();
-const password = document.querySelector('#password-login').value.trim();
+const userName = document.querySelector('username-login').value.trim();
+const password = document.querySelector('password-login').value.trim();
 
-if (email && password) {
+if (userName && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ userName, password }),
         headers: { 'Content-Type': 'application/json' },
     });
 
-    if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
-    } else {
-        alert(response.statusText);
-    }
+    // if (response.ok) {
+    //     // If successful, redirect the browser to the profile page
+    //     document.location.replace('/profile');
+    // } else {
+    //     alert(response.statusText);
+    // }
 }
 };
 
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const firstName = document.querySelector('firstname-signup').value.trim();
+    const lastName = document.querySelector('lastname-signup').value.trim();
+    const phoneNumber = document.querySelector('phonenumber-signup').value.trim();
+    const userName = document.querySelector('username-signup').value.trim();
+    const email = document.querySelector('email-signup').value.trim();
+    const password = document.querySelector('password-signup').value.trim();
 
-    if (name && email && password) {
+    if (userName && email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password}),
+            body: JSON.stringify({ firstName, lastName, phoneNumber, userName, email, password}),
             headers: {'Content-Type': 'application/json' },
         });
 
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }
+        // if (response.ok) {
+        //     document.location.replace('/profile');
+        // } else {
+        //     alert(response.statusText);
+        // }
     } 
 };
 
 document
- .querySelector('.login-form')
+ .querySelector('form login-form')
  .addEventListener('submit', loginFormHandler); 
 
  document
-  .querySelector('.signup-form')
+  .querySelector('form signup-form')
   .addEventListener('submit', signupFormHandler);
   
