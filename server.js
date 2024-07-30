d7fdd8e88767b4c5fd4ca550f1771b27afba122f
Chris21789 <98500183+Chris21789@users.noreply.github.com>
2024-07-30 16:42:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(require('./controllers/api/productRoutes'));
 // Use the routes defined in controllers
 app.use(routes);
 
